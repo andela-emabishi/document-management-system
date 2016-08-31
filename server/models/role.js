2ba015122ff90bc46668033acd2df51b5b3c2a71
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-08-31 09:59:58 +0300

    [Feature #129450183] Edit _id type for role schema

--- server/models/role.js
+++ server/models/role.js
@@ -1,18 +1,26 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+// An admin can create, edit and delete a document
+// An editor can view and edit a document
+// A viewer can only view a document and cannot edit or delete
+
+const roles = ['editor', 'viewer', 'admin'];
 
 const roleSchema = new Schema({
-  roleId: {
+  // Take _id as value generated organically by MongoDB
+
+  _id: {
     required: true,
-    type: Number,
+    type: Schema.Types.ObjectId,
     unique: true,
   },
   title: {
+    enum: roles,
     required: true,
     type: String,
-    unique: true,
   },
+
 });
 
 module.exports = mongoose.model('Role', roleSchema);
