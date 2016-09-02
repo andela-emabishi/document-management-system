274377ea13a4875472ae137235db68cd7af30cf6
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-02 16:20:11 +0300

    [Feature] Create document routes

--- server/routes/document.js
+++ server/routes/document.js
@@ -1,22 +1,70 @@
-const express = require('express');
-const DocumentController = require('../controllers/document');
+/* eslint-disable import/newline-after-import */
+/* eslint-disable no-console */
 
-const router = express.Router();
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  morgan = require('morgan'),
+  mongoose = require('mongoose'),
 
-// On all routes that end in /documents, do this:
-router.route('/documents')
-  .post(DocumentController.create),
-  .get(DocumentController.all);
+  port = process.env.PORT || 3000,
 
-router.route('/documents/:id')
-  .get(DocumentController.findById),
-  .put(DocumentController.update),
-  .delete(DocumentController.deleteById);
+    //  Require mongoose model
+  Document = require('../models/document');
 
-router.route('/documents/results')
-  .get(DocumentController.search);
+  // Connect to database
+mongoose.connect('mongodb://localhost/27017/dochero');
 
-router.route('/users/:id/documents')
-  .get(DocumentController.findByUser);
+// Configure body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-module.exports = router;
+// Configure to handle CORS requests
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
+  res.setHeader('Access-Control-Allow-Headers','X-Requested-With, content-type, Authorisation');
+  next();
+});
+
+// Use morgan to log all requests to the console
+app.use(morgan('dev'));
+
+
+app.get('/documents', function(req, res) {
+  res.send('You\'re viewing a document');
+});
+
+var documentRouter = express.Router();
+
+documentRouter.route('/documents')
+
+  .post(function(req, res) {
+    var document = new Document();
+
+    document.title = req.body.title;
+    document.content = req.body.content;
+    document.privacy = req.body.privacy;
+
+    document.save(function(err) {
+      if (err) {
+        return res.send(err);
+      }
+      else {
+        res.json({
+          success: true,
+          message: 'Document created successfully'
+        });
+      }
+    });
+
+  });
+
+// Register routes to be run by express
+app.use('/api', documentRouter);
+
+// SERVER ==================================== =============================
+// Start server
+app.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
