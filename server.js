7fa5a64abb54e76bb263319a70fd0b335e9dc939
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-05 14:36:09 +0300

    [Chore] Delete server.js file

--- server.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-
-const express = require('express'),
-  app = express(),
-  port = process.env.PORT || 8080;
-
-app.listen(port, function() {
-  console.log('Server is listening at port 8080');
-});
