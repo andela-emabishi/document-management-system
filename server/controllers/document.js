4d13210026aaebb2fa36944fe28f4d58c9d35614
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-06 13:03:40 +0300

    [Feature] Create Document Route Controller functions

---
+++ server/controllers/document.js
@@ -0,0 +1,176 @@
+const Document = require('../models/document');
+
+module.exports = {
+  create: (req, res) =>  {
+    // Create document with a unique title
+    var document = new Document();
+
+    document.title = req.body.title;
+    document.content = req.body.content;
+    document.privacy = req.body.privacy;
+    document._creatorId = req.decoded.id;
+
+    document.save(function(err) {
+      if (err) {
+        return res.send(err);
+      }
+      else {
+        res.json({
+          success: true,
+          message: 'Document created successfully'
+        });
+      }
+    });
+  },
+
+  // Get all documents
+  // TODO: Should only be able to get your documents and public documents
+
+  getAll: (req, res) => {
+    Document.find(function(err, documents) {
+      if(err) {
+        res.send(err);
+      }
+      else {
+        res.json(documents);
+      }
+    });
+  },
+
+  getDocumentById: (req, res) => {
+    Document.findById(req.params.document_id, function(err, document) {
+      if (err) {
+        // Something happened and we can't find the user
+        res.send(err);
+      }
+      else {
+        res.json(document);
+      }
+    });
+  },
+
+  updateDocumentById: function(req, res) {
+    Document.findById(req.params.document_id, function(err, document) {
+      if (err) {
+        res.send(err);
+      }
+      // Only update if a change has happened
+      if (req.body.title) document.title = req.body.title;
+      if (req.body.content) document.content = req.body.content;
+      if (req.body.privacy) document.privacy = req.body.privacy;
+
+      // Then save the user details
+      document.save(function(err) {
+        // If there's an error, tell us
+        if (err) {
+          res.send(err);
+        }
+        // Everything went well
+        else {
+          res.json({
+            success: true,
+            message: 'Document details updated successfully'
+          });
+        }
+      });
+    });
+  },
+
+  deleteDocumentById: function(req, res) {
+    Document.remove({
+      _id: req.params.document_id
+    }, function(err) {
+      if (err) {
+        return res.send(err);
+      }
+      else {
+        res.json({
+          success: true,
+          message: 'Document deleted successfully'
+        });
+      }
+    });
+  },
+
+  // Find all documents that belong to a particular user of a certain user id
+  // TODO: You should only be able to see public documents here of another creator i.e not logged in now
+  getByCreatorId: function(req, res) {
+    Document.find({_creatorId: req.decoded.id})
+          .exec(function (err, documents) {
+            if (err) {
+              res.send(err);
+              return;
+            }
+            // documents is an array of documents
+            else if (documents[0] == null) {
+              res.json({
+                message: 'No documents were found for that user'
+              });
+            } else {
+              res.send(documents);
+            }
+          });
+
+  },
+
+  // GET all documents created on a specific date (query: date, limit)
+  // TODO: Enable document fetch using date part of createdAt field
+  getByDatePublished: (req, res) => {
+    Document.find({
+      createdAt : req.params.date
+    }).limit(parseInt(req.params.limit))
+    .exec(function(err,documents) {
+      if (err) {
+        res.send(err);
+      }
+      else {
+        res.send(documents);
+      }
+    });
+  },
+
+  // A route that (query: limit) returns all the documents in order of the dates they were created (ascending or descending).
+  // It should also return results based on the limit.
+  // TODO: Merge with documents/date/limit route? If date param is null, fetch all documents regardless of created date
+  // TODO: FIX
+  getDocumentsWithLimit: (req, res) => {
+    Document.find()
+     .limit(parseInt(req.params.limit))
+     .exec(function(err,documents) {
+       if (err) {
+         res.send(err);
+       }
+       else {
+         res.send(documents);
+       }
+     });
+  },
+
+  // GET all public documents
+  // Set 'public' in route as param to enable get private docs. Document.find({privacy: req.params.public})
+  getPublicDocuments: (req, res) => {
+    Document.find({privacy: 'public'})
+      .exec(function(err, documents) {
+        if (err) {
+          res.send(err);
+          res.json({
+            success: false,
+            message: 'Cannot get private documents that are not the logged in users'
+          });
+        }
+        else {
+          res.send(documents);
+        }
+      });
+  }
+};
+
+// search: function(req, res) {
+//   // Filter document pool based on certain phrase
+//
+// },
+
+// getByRole: (req, res) => {
+//   // Find all documents that can be accessed by a certain role
+//
+// },
