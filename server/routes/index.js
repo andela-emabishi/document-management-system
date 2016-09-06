93ebfdf4abbb123a57fc55942efbf3dfef6b51d0
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-06 11:48:50 +0300

    [Feature] Move authentication protocols to routes base index file

---
+++ server/routes/index.js
@@ -0,0 +1,154 @@
+/* eslint-disable no-console */
+
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const users = require('./user');
+const documents = require('./documents');
+const roles = require('./roles');
+
+
+// Require mongoose model
+const User = require('../models/user');
+
+module.exports = function(apiRouter) {
+
+  apiRouter.get('/', function(req, res) {
+    res.json({
+      message: 'Welcome to the DOCHERO api'
+    });
+  });
+
+  apiRouter.route('/signup')
+    .post(function(req, res) {
+      var user = new User();
+
+      // Pass details of model instance to request
+      user.firstname = req.body.firstname;
+      user.lastname = req.body.lastname;
+      user.username = req.body.username;
+      user.email = req.body.email;
+      user.password = req.body.password;
+
+      user.save(function(err) {
+        if (err) {
+          if (err.code == 11000) {
+            return res.json({
+              success: false,
+              message: 'Please provide a unique username'
+            });
+          }
+          // It's another error
+          else {
+            res.send(err);
+          }
+        }
+        // No error
+        else {
+          res.json({
+            success: true,
+            message: 'User created successfully'
+          });
+        }
+      });
+    });
+
+
+  // If they have the correct password, give them a token
+  apiRouter.post('/login', function(req, res) {
+
+    // Find the user
+    // Select the password
+    User.findOne({
+      username: req.body.username
+    }).select('username password').exec(function(err, user) {
+      if (err) {
+        throw err;
+      }
+
+      // No user with that username was found
+      if (!user) {
+        res.json({
+          success: false,
+          message: 'Authentication failed. User not found'
+        });
+      }
+      // If a user with that usernmame exists
+      else {
+        if (user) {
+
+          // Check if password matches
+          var validPassword = user.comparePassword(req.body.password);
+          if (!validPassword) {
+            res.json({
+              success: false,
+              message: 'Wrong password. Failed to authenticate'
+            });
+          }
+          // If username exists and password is right
+          // Create a token
+          else {
+            var token = jwt.sign({
+              firstname: user.firstname,
+              username: user.username,
+              id: user._id
+            }, config.superSecret, {
+              // Token will expire in a day
+              expiresIn: 86400
+            });
+
+            // Return an object of the information along with the token
+            res.json({
+              success: true,
+              message: 'Enjoy your token! You\'ve just been logged in',
+              token: token
+            });
+          }
+        }
+      }
+    });
+  });
+
+  // Route middlewear to verify a token before routing a user
+  // Runs everytime a request is made
+  apiRouter.use(function(req, res, next) {
+
+    // Authenticate them
+    // Check for token
+    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+    if (token) {
+      jwt.verify(token, config.superSecret, function(err, decoded) {
+        if (err) {
+          return res.status(403).send({
+            success: false,
+            message: 'Failed to authenticate token'
+          });
+        }
+        // Everything went well, we found and verified the token
+        else {
+          console.log('Token owner');
+          console.log(decoded);
+
+          req.decoded = decoded;
+
+          next();
+        }
+      });
+    }
+    // No token was found
+    // Return 403 Access Forbidden and an error message
+    else {
+      return res.status(403).send({
+        success: false,
+        message: 'No token provided'
+      });
+    }
+  });
+
+  // users(apiRouter);
+  // documents(apiRouter);
+  roles(apiRouter);
+
+  return apiRouter;
+};
