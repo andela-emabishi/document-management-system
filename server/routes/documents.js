649911d5e9a53c297b3dcf5ac9f25fc73802f7fc
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-07 23:29:21 +0300

    [Feature] Add shared with route to document routes

--- server/routes/documents.js
+++ server/routes/documents.js
@@ -26,8 +26,12 @@ module.exports = function(router) {
   router.route('/documents/search/:search_string')
   .get(DocumentController.search);
 
-  router.route('/documents/share/:role')
+  router.route('/documents/role/:role')
   .get(DocumentController.getByRole);
+
+  // get document by shared with
+  router.route('/documents/share/:share')
+  .get(DocumentController.getBySharedWith);
 
   // Get all documents with limit and pagination offset
   // Offset === start from nth number of documents
