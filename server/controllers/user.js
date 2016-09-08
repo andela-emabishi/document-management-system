3fc30b2182643b2aafc9d877d0346297a161db4d
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-08 12:39:21 +0300

    [Feature] Add restrictions to user controller functions

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,35 +1,30 @@
 const User = require('../models/user');
+const Role = require('../models/role');
 
 module.exports = {
   // [Restricted] supra admin role only
   getAll: (req,res) => {
-    // if (req.decoded.role == 'supra-admin') {
-    //   User.find(function(err, users) {
-    //     if(err) {
-    //       res.send(err);
-    //     }
-    //     else {
-    //       res.json(users);
-    //     }
-    //   });
-    // }
-    // else {
-    //   res.json({
-    //     success: false,
-    //     message: 'Invalid operation',
-    //     status: -1
-    //   });
-    // }
-    User.find(function(err, users) {
-  if(err) {
-    res.send(err);
-  }
-  else {
-    res.json(users);
-  }
-});
+    if (req.decoded.title == 'supra-admin') {
+      User.find(function(err, users) {
+        if (err) {
+          res.send(err);
+        }
+        else {
+          res.json(users);
+        }
+      });
+    }
+    else {
+      res.json({
+        success: false,
+        message: 'Invalid operation. No access',
+        status: -1,
+      });
+    }
+
   },
 
+// [Unrestricted]
   getUserById: (req,res) => {
     User.findById(req.params.user_id, function(err, user) {
       if (err) {
@@ -42,64 +37,40 @@ module.exports = {
     });
   },
 
-// TODO:
-// Restricted to logged in user
+// [Restricted] Can only update your own documents
   updateUserById: (req,res) => {
-    // User.findById(req.params.user_id, function(err, user) {
-    //   if (err) {
-    //     res.send(err);
-    //   }
-    //   // Only update if a change has happened
-    //   if (req.body.firstname) user.firstname = req.body.firstname;
-    //   if (req.body.lastname) user.lastname = req.body.lastname;
-    //   if (req.body.username) user.username = req.body.username;
-    //   if (req.body.email) user.email = req.body.email;
-    //   if (req.body.password) user.password = req.body.password;
-    //   if (req.body.role) user.role = req.body.role;
-    //
-    //   // Then save the user details
-    //   user.save(function(err) {
-    //     // If there's an error, tell us
-    //     if (err) {
-    //       res.send(err);
-    //     }
-    //     // Everything went well
-    //     else {
-    //       res.json({
-    //         success: true,
-    //         message: 'User details updated successfully'
-    //       });
-    //     }
-    //   });
-    // });
-    User.findById(req.params.user_id)
-    // .where(req.decoded.id).equals(req.params.user_id)
-    .exec(function(err, user) {
-      if (err) {
-        res.send(err);
-      }
-     // Only update if a change has happened
-      if (req.body.firstname) user.firstname = req.body.firstname;
-      if (req.body.lastname) user.lastname = req.body.lastname;
-      if (req.body.username) user.username = req.body.username;
-      if (req.body.email) user.email = req.body.email;
-      if (req.body.password) user.password = req.body.password;
-      if (req.body.role) user.role = req.body.role;
+    User.findById(req.params.user_id, function(err, user) {
+      if (req.decoded.id == req.params.user_id) {
+        // Only update if a change has happened
+        if (req.body.firstname) user.firstname = req.body.firstname;
+        if (req.body.lastname) user.lastname = req.body.lastname;
+        if (req.body.username) user.username = req.body.username;
+        if (req.body.email) user.email = req.body.email;
+        if (req.body.password) user.password = req.body.password;
+        if (req.body.role) user.role = req.body.role;
 
-     // Then save the user details
-      user.save(function(err) {
-       // If there's an error, tell us
-        if (err) {
-          res.send(err);
-        }
-       // Everything went well
-        else {
-          res.json({
-            success: true,
-            message: 'User details updated successfully'
-          });
-        }
-      });
+        // Then save the user details
+        user.save(function(err) {
+          // If there's an error, tell us
+          if (err) {
+            res.send(err);
+          }
+          // Everything went well
+          else {
+            res.json({
+              success: true,
+              message: 'User details updated successfully'
+            });
+          }
+        });
+      }
+      else {
+        res.json({
+          success: false,
+          error: err,
+          message: 'Cannot update another users details'
+        });
+      }
     });
   },
 
