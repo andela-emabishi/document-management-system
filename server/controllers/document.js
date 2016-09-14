680e032adb95dcb366742d9b44a447c7a5e8cae6
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-14 12:56:37 +0300

    [Chore] Fix aslant errors

--- server/controllers/document.js
+++ server/controllers/document.js
@@ -2,8 +2,8 @@ const Document = require('../models/document');
 
 module.exports = {
   // Create document with a unique title
-  create: (req, res) =>  {
-    var document = new Document();
+  create: (req, res) => {
+    const document = new Document();
 
     document.title = req.body.title;
     document.content = req.body.content;
@@ -12,19 +12,18 @@ module.exports = {
     document.sharewith = req.body.sharewith;
     document.access = req.body.access;
 
-    document.save(function(err) {
+    document.save((err) => {
       if (err) {
-        return res.status(500).send({
+        res.status(500).send({
           error: err,
           message: 'Document could not be created',
-          status: '500: Internal Server Error'
+          status: '500: Internal Server Error',
         });
-      }
-      else {
+      } else {
         res.status(201).send({
           success: true,
           message: 'Document created successfully',
-          status: '201: Resource Created'
+          status: '201: Resource Created',
         });
       }
     });
@@ -35,15 +34,14 @@ module.exports = {
   getAll: (req, res) => {
     Document.find(
       {
-        $or: [ { _creatorId: req.decoded.id }, { privacy: 'public' } ]
+        $or: [{ _creatorId: req.decoded.id }, { privacy: 'public' }],
       }
     )
     .sort('-createdAt')
-    .exec(function(err, documents) {
+    .exec((err, documents) => {
       if (err) {
         res.send(err);
-      }
-      else {
+      } else {
         res.status(200).send(documents);
       }
     });
@@ -53,33 +51,31 @@ module.exports = {
   getDocumentById: (req, res) => {
     Document.findById(req.params.document_id)
     .where('_creatorId').equals(req.decoded.id)
-    .exec(function(err, documents){
+    .exec((err, documents) => {
       if (err) {
         res.send(err);
-      }
-      else if (!documents) {
+      } else if (!documents) {
         res.status(401).send({
           success: false,
           message: 'Cannot access document by that id',
-          status: '401: Unauthorised'
+          status: '401: Unauthorised',
         });
-      }
-      else {
+      } else {
         res.status(200).send(documents);
       }
     });
   },
 
   // [Restricted] Can only edit documents of logged in user
-  updateDocumentById: function(req, res) {
+  updateDocumentById: (req, res) => {
     Document.findById(req.params.document_id)
     .where('_creatorId').equals(req.decoded.id)
-    .exec(function(err, document) {
+    .exec((err, document) => {
       if (err || document === null) {
         res.status(404).send({
           error: err,
           message: 'Could not update document by the id entered',
-          status: '404: Resource Not Found'
+          status: '404: Resource Not Found',
         });
       }
        // Only update if a change has happened
@@ -90,20 +86,18 @@ module.exports = {
       if (req.body.access) document.access = req.body.access;
 
       // Then save the user details
-      document.save(function(err) {
+      document.save((err) => {
         // If there's an error, tell us
         if (err) {
           res.status(401).send({
             error: err,
             message: 'Error saving document',
-            status: '401: Unauthorised'
+            status: '401: Unauthorised',
           });
-        }
-        // Everything went well
-        else {
+        } else {
           res.status(200).send({
             success: true,
-            message: 'Document details updated successfully'
+            message: 'Document details updated successfully',
           });
         }
       });
@@ -111,30 +105,28 @@ module.exports = {
   },
 
 // [Restricted] To logged in user
-  deleteDocumentById: function(req, res) {
+  deleteDocumentById: (req, res) => {
     Document.find()
     .where('_creatorId').equals(req.decoded.id)
-    .exec(function(err) {
+    .exec((err) => {
       if (err) {
         res.send(err);
-      }
-      else {
+      } else {
         Document.remove(
           {
-            $and: [ { _creatorId: req.decoded.id }, { _id: req.params.document_id } ]
+            $and: [{ _creatorId: req.decoded.id }, { _id: req.params.document_id }],
           }
       , function(err) {
           if (err) {
             res.status(500).send({
               error: err,
               message: 'Error deleting document',
-              status: '500: Server Error'
+              status: '500: Server Error',
             });
-          }
-          else {
+          } else {
             res.status(200).send({
               success: true,
-              message: 'Document Deleted successfully if document was yours. Failed to delete another users\' documents'
+              message: 'Document Deleted successfully if document was yours. Failed to delete another users\' documents',
             });
           }
         });
@@ -144,31 +136,28 @@ module.exports = {
 
   // Find all documents that belong to a particular user of a certain user id
   // [Restricted] One must only see their own documents or public documents
-  getByCreatorId: function(req, res) {
+  getByCreatorId: (req, res) => {
     Document.find(
       {
         $or: [
-          { $and: [ {_creatorId: req.params.creator_id }, {privacy: 'public'} ] },
-          { $and: [ {_creatorId: req.decoded.id } ] },
-        ]
+          { $and: [{ _creatorId: req.params.creator_id }, { privacy: 'public' }] },
+          { $and: [{ _creatorId: req.decoded.id }] },
+        ],
       }
     )
-      .exec(function (err, documents) {
+      .exec((err, documents) => {
         if (err) {
           res.send(err);
           return;
-        }
-        // documents is an array of documents
-        else if (documents[0] == null) {
+        } else if (documents[0] == null) {
           res.status(404).send({
             message: 'No documents were found for that user. The document you are referring to may be private',
-            status: '404: Resource Not Found'
+            status: '404: Resource Not Found',
           });
         } else {
           res.status(200).send(documents);
         }
       });
-
   },
 
   // GET all documents created on a specific date (query: date, limit)
@@ -177,16 +166,15 @@ module.exports = {
   getByDatePublished: (req, res) => {
     Document.find({
       $or: [
-        { $and: [ {createdAt: req.params.date }, {privacy: 'public'} ] },
-        { $and: [ {_creatorId: req.decoded.id }, {createdAt: req.params.date } ] },
-      ]
+        { $and: [{ createdAt: req.params.date }, { privacy: 'public' }] },
+        { $and: [{ _creatorId: req.decoded.id }, { createdAt: req.params.date }] },
+      ],
     })
-    .limit(parseInt(req.params.limit))
-    .exec(function(err,documents) {
+    .limit(parseInt(req.params.limit, 10))
+    .exec((err, documents) => {
       if (err) {
         res.send(err);
-      }
-      else {
+      } else {
         res.status(200).send(documents);
       }
     });
@@ -214,16 +202,15 @@ module.exports = {
   // GET all public documents
   // Set 'public' in route as param to enable get private docs. Document.find({privacy: req.params.public})
   getPublicDocuments: (req, res) => {
-    Document.find({privacy: 'public'})
-      .exec(function(err, documents) {
+    Document.find({ privacy: 'public' })
+      .exec((err, documents) => {
         if (err) {
           res.send(err);
           res.json({
             success: false,
-            message: 'Cannot get private documents that are not the logged in users'
+            message: 'Cannot get private documents that are not the logged in users',
           });
-        }
-        else {
+        } else {
           res.send(documents);
         }
       });
@@ -235,30 +222,27 @@ module.exports = {
     // db.documents.createIndex({"title":"text","content":"text"})
     Document.find(
       {
-        $or: [  {_creatorId: req.decoded.id }, {privacy: 'public'} ]
+        $or: [{ _creatorId: req.decoded.id }, { privacy: 'public' }],
       }
     )
-    .exec(function(err) {
+    .exec((err) => {
       if (err) {
         res.send(err);
-      }
-      else {
-        Document.find({$text:{$search:req.params.search_string}},{score:{$meta:'textScore'}}).sort({score:{$meta:'textScore'}})
-        .exec(function(err, documents) {
+      } else {
+        Document.find({ $text: { $search: req.params.search_string } }, { score: { $meta: 'textScore' } }).sort({ score: { $meta: 'textScore' } })
+        .exec((documents) => {
           if (err) {
-            return res.send(err);
-          }
-          else {
+            res.send(err);
+          } else {
             // No results for the search
             if (documents[0] == null) {
-              return res.status(404).send({
+              res.status(404).send({
                 success: false,
                 message: 'No results found.',
-                status: '404: Resource Not Found'
+                status: '404: Resource Not Found',
               });
-            }
-            else {
-              return res.json(documents);
+            } else {
+              res.json(documents);
             }
           }
         });
@@ -268,13 +252,12 @@ module.exports = {
 
   getByRole: (req, res) => {
     // Find all documents that can be accessed by a certain role
-    Document.find({access: req.params.role})
-    .limit(parseInt(req.params.limit))
-    .exec(function(err, documents) {
+    Document.find({ access: req.params.role })
+    .limit(parseInt(req.params.limit, 10))
+    .exec((err, documents) => {
       if (err) {
         res.send(err);
-      }
-      else {
+      } else {
         res.json(documents);
       }
     });
