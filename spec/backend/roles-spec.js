68e306537e3d26b68e65f359cf2e5e5264c2e9b9
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-14 15:29:42 +0300

    [Feature] Add role tests

--- spec/backend/roles-spec.js
+++ spec/backend/roles-spec.js
@@ -1,9 +1,7 @@
 const app = require('../../index');
 const request = require('supertest')(app);
-const Role =  require('../../server/models/role');
 
 describe('Role tests', () => {
-
   // Before each test, log in victor hugo
   let token;
 
@@ -12,7 +10,7 @@ describe('Role tests', () => {
     .post('/api/login')
     .send({
       username: 'vichugo',
-      password: 'victorhugo'
+      password: 'victorhugo',
     })
     .end((err, res) => {
       token = res.body.token;
@@ -23,10 +21,10 @@ describe('Role tests', () => {
   it('Should validate that a new role created is unique', (done) => {
     request
       .post('/api/roles')
-      .set('x-access-token',token)
+      .set('x-access-token', token)
       .send({
         title: 'editor',
-        permission: 'readWrite'
+        permission: 'readWrite',
       })
       .end((err, res) => {
         expect(res.body.message).toBe('Please provide a unique title');
@@ -35,69 +33,54 @@ describe('Role tests', () => {
       });
   });
 
-  // it('Should validate that all users are returned', (done) => {
-  //   request
-  //   .get('/api/users')
-  //   .set('x-access-token', token)
-  //   .end((err, res) => {
-  //     expect(res.status).toBe(200);
-  //     expect(res.status).not.toBe(401);
-  //     // expect(res.body.title).toBe('supra-admin');
-  //     expect(res.body).toBeDefined();
-  //     expect(Array.isArray(res.body)).toBe(true);
-  //     done();
-  //   });
-  // });
+  it('Should validate that all roles are returned', (done) => {
+    request
+    .get('/api/roles')
+    .set('x-access-token', token)
+    .end((err, res) => {
+      expect(res.status).toBe(200);
+      expect(res.status).not.toBe(404);
+      expect(res.body).toBeDefined();
+      expect(Array.isArray(res.body)).toBe(true);
+      done();
+    });
+  });
 
-  // it('Should get a user by their id', () => {
-  //   request
-  //   .get('/api/users/57c942a8517ca48c9e5af010')
-  //   .set('x-access-token', token)
-  //   .end((err, res) => {
-  //     expect(res.status).toBe(200);
-  //     expect(res.status).not.toBe(404);
-  //     expect(res.body).toBeDefined();
-  //     expect((res.body)._id).toBeDefined();
-  //     expect((Object.keys(res.body)).length).toBeGreaterThan(0);
-  //   });
-  // });
+  it('Should validate that only a supra-admin can update role information', (done) => {
+    request
+    .put('/api/roles/65c975eb2c3d08864b51cd08')
+    .set('x-access-token', token)
+    .send({
+      title: 'super-editor',
+    })
+    .end((err, res) => {
+      expect(res.status).toBe(200);
+      expect(res.status).not.toBe(401);
+      expect(res.body).toBeDefined();
+      expect(res.body.message).toBe('Role details updated successfully');
+      done();
+    });
+  });
 
-  // it('Should validate that a user can update their own information', (done) => {
-  //   request
-  //   .put('/api/users/57c94278517ca48c9e5af00f')
-  //   .set('x-access-token', token)
-  //   .send({
-  //     username: 'victorhugo'
-  //   })
-  //   .end((err, res) => {
-  //     expect(res.status).toBe(200);
-  //     expect(res.status).not.toBe(401);
-  //     expect(res.body).toBeDefined();
-  //     expect(res.body.message).toBe('User details updated successfully');
-  //     done();
-  //   });
-  // });
+  it('Should validate that a user is able to get a role by its id', () => {
+    request
+    .get('/api/users/45c975eb2c3d08864b51cd09')
+    .set('x-access-token', token)
+    .end((err, res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.message).not.toBe('Error fetching role');
+    });
+  });
 
-  // it('Should validate that a user can be deleted by their id', (done) => {
-  //   request
-  //   .delete('/api/users/57d05aea1cd5386e0d2ca88b')
-  //   .set('x-access-token', token)
-  //   .end((err, res) => {
-  //     expect(res.status).toBe(200);
-  //     expect(res.body.status).not.toBe('401: Unauthorised');
-  //     expect(res.body.message).toBe('User deleted successfully');
-  //     done();
-  //   });
-  // });
-
-  // it('Should return an error if a user was not found', () => {
-  //   request
-  //   .get('/api/users/65c975eb2c4d08864b51cd08')
-  //   .set('x-access-token', token)
-  //   .end((err, res) => {
-  //     expect(res.status).toBe(404);
-  //     expect(res.body.status).toBe('404: Resource Not Found');
-  //   });
-  // });
-
+  it('Should validate that a role can be deleted', (done) => {
+    request
+    .delete('/api/roles/45c975eb2c3d08864b51cd08')
+    .set('x-access-token', token)
+    .end((err, res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.status).not.toBe('401: Unauthorised');
+      expect(res.body.message).toBe('Role deleted successfully');
+      done();
+    });
+  });
 });
