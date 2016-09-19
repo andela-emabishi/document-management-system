ae752ebfad28db56c5734540a2f49e822f658888
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-19 13:11:13 +0300

    [Chore] Refactor code

--- server/models/role.js
+++ server/models/role.js
@@ -2,14 +2,8 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-// read == read all public documents + their own & assigned documents? OR read ALL documents?
-//  readWrite === read all public documents + their own, write assigned documents(private or public) OR read& write all documents
-// readWriteDelete === read,edit and delete public documents + assigned private documents
-// delete: admin === delete hashed private documents
-
 const roleSchema = new Schema({
   title: {
-    // enums: roleTypes,
     required: true,
     type: String,
     unique: true,
