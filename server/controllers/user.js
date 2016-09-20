b1ad3aa373d660a2ef719eb97a3f4894fb8c94de
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-20 17:37:59 +0300

    [Chore] Refactor HTTP responses

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -7,7 +7,6 @@ module.exports = {
       User.find((err, users) => {
         if (err) {
           res.status(404).send({
-            success: false,
             error: err,
             status: '404: Resource Not Found',
           });
@@ -17,14 +16,12 @@ module.exports = {
       });
     } else {
       res.status(401).send({
-        success: false,
         message: 'Invalid operation. No access',
         status: '401: Unauthorised',
       });
     }
   },
 
-// [Unrestricted]
   getUserById: (req, res) => {
     User.findById(req.params.user_id, (err, user) => {
       if (err) {
@@ -33,7 +30,6 @@ module.exports = {
         });
       } else if (!user) {
         res.status(404).send({
-          success: false,
           message: 'No user by that id found',
           status: '404: Resource Not Found',
         });
@@ -62,14 +58,12 @@ module.exports = {
             res.send(err);
           } else {
             res.status(200).send({
-              success: true,
               message: 'User details updated successfully',
             });
           }
         });
       } else {
         res.status(401).send({
-          success: false,
           error: err,
           message: 'Cannot update another users details',
           status: '401: Unauthorised',
@@ -93,14 +87,12 @@ module.exports = {
           });
         } else {
           res.status(200).send({
-            success: true,
             message: 'User deleted successfully',
           });
         }
       });
     } else {
       res.status(401).send({
-        success: false,
         message: 'Cannot delete another user. Can only delete yourself',
         status: '401: Unauthorised',
       });
