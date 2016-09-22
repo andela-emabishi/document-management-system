2b24867143fe00f903c97046847faf8797877c92
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-22 21:49:18 +0300

    [Refactor] Modify search controller

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,12 +1,6 @@
 const SearchController = require('../controllers/search');
 
 module.exports = (router) => {
-  router.route('/search/date')
-  .get(SearchController.getByDatePublished);
-
-  router.route('/search/string')
+  router.route('/search')
   .get(SearchController.search);
-
-  router.route('/search/role')
-  .get(SearchController.getByRole);
 };
