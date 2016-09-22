14b729a2f8af26ef3e2424a4d1b8827816bf3f9e
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-22 21:48:46 +0300

    [Feature] Modify search query

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,21 +1,37 @@
 const Document = require('../models/document');
 
 module.exports = {
-    // GET all documents created on a specific date (query: date, limit)
-    //  [Restricted] Cannot get private documents that belong to other users
-  getByDatePublished: (req, res) => {
+  search: (req, res) => {
     const startDate = new Date(req.query.date);
     // i.e. One day after the start date
     const endDate = new Date(startDate.getTime() + (24 * 60 * 60 * 1000));
-
-    Document.find({
+    const $query = {
       $and: [
         { $or: [{ _creatorId: req.decoded.id }, { privacy: 'public' }] },
-        { createdAt: { $gte: startDate, $lt: endDate } },
       ],
-    })
+    };
+    // Search by date published
+    //  [Restricted] Cannot get private documents that belong to other users
+    if (req.query.date) {
+      const dateQuery = { createdAt: { $gte: startDate, $lt: endDate } };
+      $query.$and.push(dateQuery);
+    }
+    /* [Restricted]: Able to search logged in users' documents
+    * and public documents title and content
+    */
+    if (req.query.q) {
+      const textQuery = { $text: { $search: req.query.q } };
+      $query.$and.push(textQuery);
+    }
+    // Find all documents that can be accessed by a certain role
+    if (req.query.role) {
+      $query.access = req.query.role;
+    }
+
+    Document.find($query)
     .sort('-createdAt')
-    .limit(parseInt(req.query.limit, 10))
+    .skip(parseInt(req.query.offset || 0, 10))
+    .limit(parseInt(req.query.limit || 10, 10))
     .exec((err, documents) => {
       if (err) {
         res.status(500).send({
@@ -25,57 +41,7 @@ module.exports = {
         });
       } else if (documents.length === 0) {
         res.status(404).send({
-          message: 'No documents found that were published on that date',
-          status: '404: Resource Not Found',
-        });
-      } else {
-        res.status(200).send(documents);
-      }
-    });
-  },
-
-  // [Restricted]: Able to search logged in users' documents and public documents title and content
-  search: (req, res) => {
-    Document.find(
-      {
-        $or: [{ _creatorId: req.decoded.id }, { privacy: 'public' }],
-      }
-    )
-    .exec((err) => {
-      if (err) {
-        res.send(err);
-      } else {
-        Document.find({ $text: { $search: req.query.search_string } }, { score: { $meta: 'textScore' } }).sort({ score: { $meta: 'textScore' } })
-        .exec(function(err, documents) {
-          if (err) {
-            res.send(err);
-          } else if (documents.length === 0) {
-            res.status(404).send({
-              message: 'No results found.',
-              status: '404: Resource Not Found',
-            });
-          } else {
-            res.status(200).send(documents);
-          }
-        });
-      }
-    });
-  },
-
-  // Find all documents that can be accessed by a certain role
-  getByRole: (req, res) => {
-    Document.find({ access: req.query.role })
-    .limit(parseInt(req.query.limit, 10))
-    .exec((err, documents) => {
-      if (err) {
-        res.send({
-          error: err,
-          message: 'Error fetching documents',
-          status: '500: Server Error',
-        });
-      } else if (documents.length === 0) {
-        res.status(404).send({
-          message: 'No documents available to that role',
+          message: 'No documents found.',
           status: '404: Resource Not Found',
         });
       } else {
