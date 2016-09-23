0a5806388b320f01cf7cf87682c25ab1e2d9c81e
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-23 23:44:03 +0300

    [Refactor] Search controller

--- spec/backend/search-spec.js
+++ spec/backend/search-spec.js
@@ -7,75 +7,78 @@ describe('Search tests', () => {
 
   beforeAll((done) => {
     request
-    .post('/api/login')
-    .send({
-      username: 'vichugo',
-      password: 'victorhugo',
-    })
-    .end((err, res) => {
-      token = res.body.token;
-      done();
-    });
+      .post('/api/login')
+      .send({
+        username: 'vichugo',
+        password: 'victorhugo',
+      })
+      .end((err, res) => {
+        token = res.body.token;
+        done();
+      });
   });
 
   describe('Search tests', () => {
-    it('Should return all documents limited by a specific number that can be accessed by a specified role', (done) => {
+    it('Should return all documents limited by a specific number that can be '
+    + 'accessed by a specified role', (done) => {
       request
-      .get('/api/search/role?role=55c975eb2c3d08864b51cd08&limit=1')
+        .get('/api/documents?role=55c975eb2c3d08864b51cd08&limit=1')
+        .set('x-access-token', token)
+        .end((err, res) => {
+          expect(res.status).toBe(200);
+          expect(res.status).not.toBe(404);
+          expect(res.body).toBeDefined();
+          expect((Object.keys(res.body)).length).toBe(1);
+          done();
+        });
+    });
+  });
+
+  it('Should return documents published on a specific date when '
+  + 'searched by publishing date', (done) => {
+    request
+    .get('/api/docuement?date=2016-09-16')
       .set('x-access-token', token)
       .end((err, res) => {
         expect(res.status).toBe(200);
-        expect(res.status).not.toBe(404);
         expect(res.body).toBeDefined();
-        expect((Object.keys(res.body)).length).toBe(1);
+        expect(Array.isArray(res.body)).toBe(true);
+        expect(res.body.length).not.toBe(0);
         done();
       });
-    });
   });
 
-  it('Should return documents published on a specific date when searched by publishing date', (done) => {
+  it('Should return a message if documents published on a'
+  + 'specific date when searched were not found', (done) => {
     request
-    .get('/api/search/date?date=2016-09-16')
-    .set('x-access-token', token)
-    .end((err, res) => {
-      expect(res.status).toBe(200);
-      expect(res.body).toBeDefined();
-      expect(Array.isArray(res.body)).toBe(true);
-      expect(res.body.length).not.toBe(0);
-      done();
-    });
-  });
-
-  it('Should return a message if documents published on a specific date when searched were not found', (done) => {
-    request
-    .get('/api/search/date?date=2000-09-15')
-    .set('x-access-token', token)
-    .end((err, res) => {
-      expect(res.status).toBe(404);
-      expect(res.body.status).toBe('404: Resource Not Found');
-      done();
-    });
+      .get('/api/docuement?date=2000-09-15')
+      .set('x-access-token', token)
+      .end((err, res) => {
+        expect(res.status).toBe(404);
+        expect(res.body.status).toBe('404: Resource Not Found');
+        done();
+      });
   });
 
   it('Should return results for a search query if words are found', (done) => {
     request
-    .get('/api/search/string?search_string=Quasimodo')
-    .set('x-access-token', token)
-    .end((err, res) => {
-      expect(res.status).toBe(200);
-      expect((res.body[0].content).includes('Quasimodo')).toBe(true);
-      done();
-    });
+      .get('/api/documents?q=Quasimodo')
+      .set('x-access-token', token)
+      .end((err, res) => {
+        expect(res.status).toBe(200);
+        expect((res.body[0].content).includes('Quasimodo')).toBe(true);
+        done();
+      });
   });
 
   it('Should return a message if no results for a search query have been found', (done) => {
     request
-    .get('/api/search/string?search_string=Quasi')
-    .set('x-access-token', token)
-    .end((err, res) => {
-      expect(res.status).toBe(404);
-      expect(res.body.status).toBe('404: Resource Not Found');
-      done();
-    });
+      .get('/api/documents?q=Quasi')
+      .set('x-access-token', token)
+      .end((err, res) => {
+        expect(res.status).toBe(404);
+        expect(res.body.status).toBe('404: Resource Not Found');
+        done();
+      });
   });
 });
