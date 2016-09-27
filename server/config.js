0f7cf23e19e397277c2481e73bb6651de997db30
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-27 19:01:11 +0300

    [Fix] Add dochero as production database

--- server/config.js
+++ server/config.js
@@ -2,7 +2,8 @@ module.exports = {
   development: {
     port: process.env.PORT || 3000,
     superSecret: 'iloveexistentialquestionslikewhydoweexist',
-    database: 'mongodb://localhost:27017/27017',
+    // database: 'mongodb://localhost:27017/27017',
+    database: 'mongodb://localhost:27017/dochero',
   },
   test: {
     port: process.env.PORT || 8080,
