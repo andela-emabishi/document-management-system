3dae8aab2e11730b0d615ffc507d8d4568e2ae0a
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-09-27 21:39:53 +0300

    [Refactor] Add exit mongo line

--- mongo-prod.js
+++ mongo-prod.js
@@ -2,3 +2,4 @@ use dochero
 // Now using 27017
 // db.messages.dropIndex("title_text_content_text")
 db.documents.createIndex({"title":"text","content":"text" })
+exit
--- mongo-test.js
+++ mongo-test.js
@@ -1,3 +1,4 @@
 use dochero-test
 // db.messages.dropIndex("title_text_content_text")
 db.documents.createIndex({"title":"text","content":"text" })
+exit
